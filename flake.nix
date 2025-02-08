9987d3e410261b79c38cbfc7219750e2839b2790
aura-ix <173636334+aura-ix@users.noreply.github.com>
2025-02-08 14:37:24 -0500

    repair nix flake

--- flake.nix
+++ flake.nix
@@ -4,26 +4,37 @@
 
   outputs = { self, nixpkgs, poetry2nix }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
-      in {
-        default = mkPoetryApplication { projectDir = self; };
+      override = (self: super: {
+        piazza-api = super.piazza-api.overridePythonAttrs (old: {
+          buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+        });
       });
+    in {
+      packages = forAllSystems (system:
+        let p2n = poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; };
+        in {
+          default = p2n.mkPoetryApplication {
+            projectDir = self;
+            overrides = p2n.defaultPoetryOverrides.extend override;
+          };
+        });
 
-      devShells = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
-      in {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            (mkPoetryEnv { projectDir = self; })
-            poetry
-          ];
-        };
-      });
+      devShells = forAllSystems (system:
+        let p2n = poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; };
+        in {
+          default = pkgs.${system}.mkShellNoCC {
+            packages = with pkgs.${system}; [
+              (p2n.mkPoetryEnv {
+                projectDir = self;
+                overrides = p2n.defaultPoetryOverrides.extend override;
+              })
+              poetry
+            ];
+          };
+        });
     };
 }
